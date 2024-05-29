3a27cb14a2500a413ecf9ec8ca77caafdb385ed4
orero002 <oreromark1@gmail.com>
2024-05-29 09:23:07 -0700

    program.c

---
+++ drake.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main ()
+{
+    int principal;
+    int interestrate;
+    int timescompounded;
+    int time; 
+
+    printf("please enter the principle:\n");
+    scanf("%d",&principal);
+
+    printf("please enter interest rate:\n");
+    scanf("%d", &interestrate);
+
+    printf("please enter times compounded\n");
+    scanf("%d", &timescompounded);
+
+    printf("please enter the time in years\n");
+    scanf("%d", &time);
+
+
+    return 0;
+}
