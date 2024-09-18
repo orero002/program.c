6d3e5f9f4acbd6872b5bcc505ec74bd5f504c413
orero002 <oreromark1@gmail.com>
2024-09-18 12:30:17 -0700

    first ++ commit

---
+++ sec.c
@@ -0,0 +1,21 @@
+// printing the largest number 
+#include <iostream> 
+using namespace std;
+
+int main(){
+ int num1,num2, num3;
+
+count << enter first number << endl;
+cin>> num1
+
+count << enter second number << endl; 
+cin >>num2
+
+count << enter third number << endl;
+cin >> num3
+
+count >>enter numbers  of three employees>>
+cin>>num1>>num2>>num3;
+
+    return 0;
+}
